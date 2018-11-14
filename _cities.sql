c23a1d8750e0f802f117aba38cf36d687f7209c4
fc115240 <fc115240@yandex.ru>
2018-11-14 23:07:07 +0300

    урок 2

---
+++ _cities.sql
@@ -0,0 +1,5 @@
+SET NAMES utf8 ;
+INSERT INTO `geodata`.`_cities` (`country_id`, `region_id`, `title`, `important`) VALUES (1, 1, 'Москва', true);
+INSERT INTO `geodata`.`_cities` (`country_id`, `region_id`, `title`, `important`) VALUES (1, 2, 'Сыктывкар', true);
+INSERT INTO `geodata`.`_cities` (`country_id`, `region_id`, `title`, `important`) VALUES (1, 4, 'Киров', true);
+INSERT INTO `geodata`.`_cities` (`country_id`, `region_id`, `title`, `important`) VALUES (4, 5, 'Судеты', true);
---
+++ _countries.sql
@@ -0,0 +1,5 @@
+SET NAMES utf8 ;
+INSERT INTO `geodata`.`_countries` (`title`) VALUES ('Россия');
+INSERT INTO `geodata`.`_countries` (`title`) VALUES ('Белорусия');
+INSERT INTO `geodata`.`_countries` (`title`) VALUES ('Казахстан');
+INSERT INTO `geodata`.`_countries` (`title`) VALUES ('Чехия');
