926fdb58d53b5538e8ccccca2ef6585390fb7167
fc115240 <fc115240@yandex.ru>
2018-11-24 14:11:28 +0300

    убираем лишнее

--- _countries.sql
+++
@@ -1,5 +0,0 @@
-SET NAMES utf8 ;
-INSERT INTO `geodata`.`_countries` (`title`) VALUES ('Россия');
-INSERT INTO `geodata`.`_countries` (`title`) VALUES ('Белорусия');
-INSERT INTO `geodata`.`_countries` (`title`) VALUES ('Казахстан');
-INSERT INTO `geodata`.`_countries` (`title`) VALUES ('Чехия');
--- _regions.sql
+++
@@ -1,6 +0,0 @@
-SET NAMES utf8 ;
-INSERT INTO `geodata`.`_regions` (`country_id`, `title`) VALUES (1, 'Центральный федеральный округ');
-INSERT INTO `geodata`.`_regions` (`country_id`, `title`) VALUES (1, 'Республика Коми');
-INSERT INTO `geodata`.`_regions` (`country_id`, `title`) VALUES (1, 'Республика Тыва');
-INSERT INTO `geodata`.`_regions` (`country_id`, `title`) VALUES (1, 'Кировская область');
-INSERT INTO `geodata`.`_regions` (`country_id`, `title`) VALUES (4, 'Судетская область');
